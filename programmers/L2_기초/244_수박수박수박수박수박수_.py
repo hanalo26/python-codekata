7ea1cb1e722f3f996caeb62b188773e417619d8e
hanalo26 <skyoung100@gmail.com>
2026-01-16 01:53:37 +0900

    [프로그래머스] L2 - 수박수박수박수박수박수? (#244)

---
+++ programmers/L2_기초/244_수박수박수박수박수박수_.py
@@ -0,0 +1,9 @@
+# 수박수박수박수박수박수?
+# 프로그래머스 L2 (기초)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12922
+# 알고리즘: 문자열
+# 작성자: 백하은
+# 작성일: 2026. 01. 16. 01:53:37
+
+def solution(n):
+    return "수박"*(n//2) + "수"*(n%2)
