5fd3469dadfb965f58916788b8caeb6eb3654b64
hanalo26 <skyoung100@gmail.com>
2026-01-16 01:47:58 +0900

    [프로그래머스] L1 - 문자열을 정수로 바꾸기 (#231)

---
+++ programmers/L1_입문/231_문자열을_정수로_바꾸기.py
@@ -0,0 +1,9 @@
+# 문자열을 정수로 바꾸기
+# 프로그래머스 L1 (입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12925
+# 알고리즘: 기본연산, 형변환
+# 작성자: 백하은
+# 작성일: 2026. 01. 16. 01:47:57
+
+def solution(s):
+    return int(s)
