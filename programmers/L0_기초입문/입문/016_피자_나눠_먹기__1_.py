0d3cfe336c2631dac76952958d5769db85e1718d
hanalo26 <skyoung100@gmail.com>
2026-01-19 10:44:04 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

---
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -0,0 +1,11 @@
+# 피자 나눠 먹기 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+# 알고리즘: 기초
+# 작성자: 백하은
+# 작성일: 2026. 01. 19. 10:44:03
+
+from math import ceil
+
+def solution(n):
+    return ceil(n/7)
