175b15d168c35ec3550f429b06296c8f95f6f4b8
hanalo26 <skyoung100@gmail.com>
2026-01-23 09:24:37 +0900

    [프로그래머스] L0 - 모음 제거 (#33)

---
+++ programmers/L0_기초입문/입문/033_모음_제거.py
@@ -0,0 +1,13 @@
+# 모음 제거
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120849
+# 알고리즘: 기초
+# 작성자: 백하은
+# 작성일: 2026. 01. 23. 09:24:36
+
+def solution(my_string):
+    answer = ''
+    for alpha in my_string:
+        if alpha not in 'aeiou':
+            answer += alpha
+    return answer
