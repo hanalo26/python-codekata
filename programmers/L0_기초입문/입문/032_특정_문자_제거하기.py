389d6e9d69d02fd6af64c68174e4868fbe67ba53
hanalo26 <skyoung100@gmail.com>
2026-01-23 09:20:55 +0900

    [프로그래머스] L0 - 특정 문자 제거하기 (#32)

---
+++ programmers/L0_기초입문/입문/032_특정_문자_제거하기.py
@@ -0,0 +1,9 @@
+# 특정 문자 제거하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120826
+# 알고리즘: 기초
+# 작성자: 백하은
+# 작성일: 2026. 01. 23. 09:20:54
+
+def solution(my_string, letter):
+    return my_string.replace(letter,'')
