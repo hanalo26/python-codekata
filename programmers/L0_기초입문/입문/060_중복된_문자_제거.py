3a4eebf3998de1b20ef86a9cb9f33a65a8632b24
hanalo26 <skyoung100@gmail.com>
2026-02-24 11:12:45 +0900

    [프로그래머스] L0 - 중복된 문자 제거 (#60)

---
+++ programmers/L0_기초입문/입문/060_중복된_문자_제거.py
@@ -0,0 +1,15 @@
+# 중복된 문자 제거
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120888
+# 알고리즘: 기초
+# 작성자: 백하은
+# 작성일: 2026. 02. 24. 11:12:44
+
+def solution(my_string):
+    answer = ''
+    
+    for i in my_string:
+        if i not in answer:
+            answer += i
+    
+    return answer
