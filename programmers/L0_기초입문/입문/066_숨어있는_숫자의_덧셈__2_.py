b02325d215e49e2991ec5595fcc15e830447d600
hanalo26 <skyoung100@gmail.com>
2026-03-16 10:02:36 +0900

    [프로그래머스] L0 - 숨어있는 숫자의 덧셈 (2) (#66)

--- programmers/L0_기초입문/입문/066_숨어있는_숫자의_덧셈__2_.py
+++ programmers/L0_기초입문/입문/066_숨어있는_숫자의_덧셈__2_.py
@@ -3,10 +3,12 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120864
 # 알고리즘: 기초
 # 작성자: 백하은
-# 작성일: 2026. 03. 16. 10:01:31
+# 작성일: 2026. 03. 16. 10:02:35
 
+# 정규표현식 기능을 쓰기 위한 모듈 호출
 import re
 
+# 문제 풀이
 def solution(my_string):
     
     nums = re.findall(r'\d+', my_string)
