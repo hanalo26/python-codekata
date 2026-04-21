90186f7c2d216a01ce5194a89da12a8a44de8172
hanalo26 <skyoung100@gmail.com>
2026-04-21 09:33:21 +0900

    [프로그래머스] L0 - 문자 리스트를 문자열로 변환하기 (#102)

---
+++ programmers/L0_기초입문/기초트레이닝/102_문자_리스트를_문자열로_변환하기.py
@@ -0,0 +1,10 @@
+# 문자 리스트를 문자열로 변환하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181941
+# 알고리즘: 연산
+# 작성자: 백하은
+# 작성일: 2026. 04. 21. 09:33:20
+
+def solution(arr):
+    answer = ''.join(arr)
+    return answer
