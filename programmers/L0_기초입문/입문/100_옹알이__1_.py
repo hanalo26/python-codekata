9e48cbfce39d722bcc8ac0a0930beaa2a3692e08
hanalo26 <skyoung100@gmail.com>
2026-04-29 16:43:32 +0900

    [프로그래머스] L0 - 옹알이 (1) (#100)

--- programmers/L0_기초입문/입문/100_옹알이__1_.py
+++ programmers/L0_기초입문/입문/100_옹알이__1_.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120956
 # 알고리즘: 기초
 # 작성자: 백하은
-# 작성일: 2026. 04. 29. 16:42:41
+# 작성일: 2026. 04. 29. 16:43:32
 
 def solution(babbling):
     answer = 0
@@ -12,7 +12,9 @@ def solution(babbling):
     for bab in babbling:
         for word in can_speak:
             bab = bab.replace(word, " ")
-            
+        
+        # 모든 단어를 훑은 뒤, 공백을 제거(.strip())했을 때
+        # 아무것도 남지 않아야 조카가 완벽하게 발음한 것
         if bab.strip() == "":
             answer += 1
     
