2a3b0420d0893ef22cdfbb594ae41b8643b37adc
hanalo26 <skyoung100@gmail.com>
2026-04-30 10:13:48 +0900

    [프로그래머스] L0 - 로그인 성공? (#83)

---
+++ programmers/L0_기초입문/입문/083_로그인_성공_.py
@@ -0,0 +1,17 @@
+# 로그인 성공?
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120883
+# 알고리즘: 기초
+# 작성자: 백하은
+# 작성일: 2026. 04. 30. 10:13:47
+
+def solution(id_pw, db):
+    user_id, user_pw = id_pw
+    
+    for db_id, db_pw in db:
+        if user_id == db_id:
+            if user_pw == db_pw:
+                return 'login'
+            else:
+                return "wrong pw"
+    return "fail"
