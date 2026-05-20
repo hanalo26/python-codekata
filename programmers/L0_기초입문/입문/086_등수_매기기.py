767a8620a1e292d5689f0703f8cbe621bfaa0be2
hanalo26 <skyoung100@gmail.com>
2026-05-20 10:40:56 +0900

    [프로그래머스] L0 - 등수 매기기 (#86)

--- programmers/L0_기초입문/입문/086_등수_매기기.py
+++ programmers/L0_기초입문/입문/086_등수_매기기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120882
 # 알고리즘: 기초
 # 작성자: 백하은
-# 작성일: 2026. 05. 20. 10:39:55
+# 작성일: 2026. 05. 20. 10:40:55
 
 def solution(score):
     # 각 학생들의 평균 점수가 담긴 리스트
@@ -11,4 +11,6 @@ def solution(score):
     
     # 평균점수를 내림차순으로 정렬
     answer = sorted(total_score, reverse=True)
+    
+    # total_score에서는 몇 번째 원소였는지 찾아서 반환 (단, 인덱스는 0부터이므로 +1 추가)
     return [answer.index(i)+1 for i in total_score]
