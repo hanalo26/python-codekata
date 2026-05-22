cae2df46cfff4fe711ac47b933fde0eee4263865
hanalo26 <skyoung100@gmail.com>
2026-05-22 13:52:43 +0900

    [프로그래머스] L0 - 유한소수 판별하기 (#89)

---
+++ programmers/L0_기초입문/입문/089_유한소수_판별하기.py
@@ -0,0 +1,24 @@
+# 유한소수 판별하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120878
+# 알고리즘: 기초
+# 작성자: 백하은
+# 작성일: 2026. 05. 22. 13:52:42
+
+import math
+
+def solution(a, b):
+    # math.gcd(a,b) - a,b의 최대공약수를 구함
+    c = math.gcd(a,b)
+    
+    a = a//c
+    b = b//c
+    
+    # 약분한 후, 분모에 2 또는 5만 있어야 유한소수임
+    while b % 2 == 0:
+        b = b // 2
+        
+    while b % 5 == 0:
+        b = b // 5
+        
+    return 1 if b == 1 else 2
