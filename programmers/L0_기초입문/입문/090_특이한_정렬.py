a877b31c62bd208e2752878e3de158ece9d0cf3f
hanalo26 <skyoung100@gmail.com>
2026-05-26 09:57:03 +0900

    [프로그래머스] L0 - 특이한 정렬 (#90)

--- programmers/L0_기초입문/입문/090_특이한_정렬.py
+++ programmers/L0_기초입문/입문/090_특이한_정렬.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120880
 # 알고리즘: 기초
 # 작성자: 백하은
-# 작성일: 2026. 05. 26. 09:55:56
+# 작성일: 2026. 05. 26. 09:57:02
 
 def solution(numlist, n):
     # numlist를 오름차순으로 정렬하되, lambda 함수를 써서 절댓값, -x 기준 순서로 정렬
