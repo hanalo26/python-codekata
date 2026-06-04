c7975952becce29c6a65588b6269192e4e2ab49e
hanalo26 <skyoung100@gmail.com>
2026-06-04 21:58:24 +0900

    [프로그래머스] L0 - 연속된 수의 합 (#95)

--- programmers/L0_기초입문/입문/095_연속된_수의_합.py
+++ programmers/L0_기초입문/입문/095_연속된_수의_합.py
@@ -3,9 +3,9 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120923
 # 알고리즘: 기초
 # 작성자: 백하은
-# 작성일: 2026. 06. 04. 21:57:43
+# 작성일: 2026. 06. 04. 21:58:24
 
 def solution(num, total):
-    # 등차수열 합 공식 중 처음과 끝을 알 때 사용하는 식을 전개해서 첫 번째 항(start)을 구함
+    # 전체 합(total)에서 0부터 늘어난 누적합을 뺀 뒤, 개수(num)로 나누어 첫 번째 항을 구함
     start = (total - sum(range(num))) // num
     return [i for i in range(start, start + num)]
