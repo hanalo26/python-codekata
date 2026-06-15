d57e171d82c5a07d1980d8177777205b32837a7d
hanalo26 <skyoung100@gmail.com>
2026-06-15 16:12:56 +0900

    110_문자열_곱하기

--- programmers/L0_기초입문/기초트레이닝/110_문자열_곱하기.py
+++ programmers/L0_기초입문/기초트레이닝/110_문자열_곱하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 L0 (기초·입문)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181940
 # 알고리즘: 연산
-# 작성자: 로그
+# 작성자: 백하은
 # 작성일: 2026. 06. 15. 16:08:38
 
 def solution(my_string, k):
