7cd692c09772da122b1b35eef457f16033aaf723
hanalo26 <skyoung100@gmail.com>
2026-06-17 14:06:26 +0900

    [프로그래머스] L0 - 카운트 업 (#114)

---
+++ programmers/L0_기초입문/기초트레이닝/114_카운트_업.py
@@ -0,0 +1,11 @@
+# 카운트 업
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181920
+# 알고리즘: 반복문
+# 작성자: 백하은
+# 작성일: 2026. 06. 17. 14:06:26
+
+def solution(start_num, end_num):
+    
+    # start_num부터 end_num까지의 숫자를 리스트로 만들어 반환
+    return list(range(start_num, end_num+1))
