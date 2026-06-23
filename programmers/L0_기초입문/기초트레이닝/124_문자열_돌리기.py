75667ceba3d07ba7b4ac7b380b71c151a9bed249
hanalo26 <skyoung100@gmail.com>
2026-06-23 21:07:57 +0900

    [프로그래머스] L0 - 문자열 돌리기 (#124)

--- programmers/L0_기초입문/기초트레이닝/124_문자열_돌리기.py
+++ programmers/L0_기초입문/기초트레이닝/124_문자열_돌리기.py
@@ -3,9 +3,10 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181945
 # 알고리즘: 출력
 # 작성자: 백하은
-# 작성일: 2026. 06. 23. 21:07:36
+# 작성일: 2026. 06. 23. 21:07:57
 
 str = input()
 
+# 문자열에서 글자를 하나씩 꺼내 출력
 for i in str:
     print(i)
