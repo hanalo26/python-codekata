a6cb7c75ce516861d30bd616d8c40bee20733135
hanalo26 <skyoung100@gmail.com>
2026-06-25 19:57:23 +0900

    [프로그래머스] L0 - 정수 부분 (#129)

--- programmers/L0_기초입문/기초트레이닝/129_정수_부분.py
+++ programmers/L0_기초입문/기초트레이닝/129_정수_부분.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181850
 # 알고리즘: 함수(메서드)
 # 작성자: 백하은
-# 작성일: 2026. 06. 25. 19:45:38
+# 작성일: 2026. 06. 25. 19:57:22
 
 def solution(flo):
     return int(flo)
