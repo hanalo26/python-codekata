deb4ea4b276ede5526c1a7882e00a4971f3c6e0c
hanalo26 <skyoung100@gmail.com>
2026-06-26 12:14:45 +0900

    [프로그래머스] L0 - 문자열로 변환 (#137)

---
+++ programmers/L0_기초입문/기초트레이닝/137_문자열로_변환.py
@@ -0,0 +1,9 @@
+# 문자열로 변환
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181845
+# 알고리즘: 함수(메서드)
+# 작성자: 백하은
+# 작성일: 2026. 06. 26. 12:14:45
+
+def solution(n):
+    return str(n)
