d2d15e206db9cd500f3331729ea750bfb7953c05
hanalo26 <skyoung100@gmail.com>
2026-06-26 11:57:49 +0900

    [프로그래머스] L0 - 0 떼기 (#134)

---
+++ programmers/L0_기초입문/기초트레이닝/134_0_떼기.py
@@ -0,0 +1,10 @@
+# 0 떼기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181847
+# 알고리즘: 함수(메서드)
+# 작성자: 백하은
+# 작성일: 2026. 06. 26. 11:57:49
+
+def solution(n_str):
+    answer = str(int(n_str))
+    return answer
