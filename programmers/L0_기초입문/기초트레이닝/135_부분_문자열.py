8adeaab84da6df6a05a2f22ff7bcfe43c0c5cca1
hanalo26 <skyoung100@gmail.com>
2026-06-26 11:59:34 +0900

    [프로그래머스] L0 - 부분 문자열 (#135)

---
+++ programmers/L0_기초입문/기초트레이닝/135_부분_문자열.py
@@ -0,0 +1,12 @@
+# 부분 문자열
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181842
+# 알고리즘: 조건문 활용
+# 작성자: 백하은
+# 작성일: 2026. 06. 26. 11:59:34
+
+def solution(str1, str2):
+    if str1 in str2:
+        return 1
+    else:
+        return 0
