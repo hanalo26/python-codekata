f745a43311ac7c3f9b2400a4575c98616b150712
hanalo26 <skyoung100@gmail.com>
2026-06-28 18:47:13 +0900

    [프로그래머스] L0 - 배열의 길이에 따라 다른 연산하기 (#142)

--- programmers/L0_기초입문/기초트레이닝/142_배열의_길이에_따라_다른_연산하기.py
+++ programmers/L0_기초입문/기초트레이닝/142_배열의_길이에_따라_다른_연산하기.py
@@ -3,12 +3,12 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181854
 # 알고리즘: 함수(메서드)
 # 작성자: 백하은
-# 작성일: 2026. 06. 28. 18:47:02
+# 작성일: 2026. 06. 28. 18:47:13
 
 def solution(arr, n):
     arr_length = len(arr)
     
-    # 배열의 길이가 짝수 -> 짝수 인덱스에 존재하는 숫자에 +n
+    # 배열의 길이가 짝수 -> 홀수 인덱스에 존재하는 숫자에 +n
     if arr_length % 2 == 0:
         for i in range(1,arr_length,2):
             arr[i] = arr[i] + n
