d1742eefa7757ff852d4ead8bf27a092c946f708
hanalo26 <skyoung100@gmail.com>
2026-06-29 16:50:31 +0900

    [프로그래머스] L0 - x 사이의 개수 (#147)

--- programmers/L0_기초입문/기초트레이닝/147_x_사이의_개수.py
+++ programmers/L0_기초입문/기초트레이닝/147_x_사이의_개수.py
@@ -3,13 +3,14 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181867
 # 알고리즘: 문자열
 # 작성자: 백하은
-# 작성일: 2026. 06. 29. 16:50:08
+# 작성일: 2026. 06. 29. 16:50:30
 
 def solution(myString):
     answer = []
     
     # 'x'를 기준으로 문자열을 나눔
     for sub_str in myString.split('x'):
+        # 분리한 문자열의 길이 추가
         answer.append(len(sub_str))
     
     return answer
