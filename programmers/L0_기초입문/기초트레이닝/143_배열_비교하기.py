3c5c4b7ffe8af3e94d6f518b23c92a29d632b625
hanalo26 <skyoung100@gmail.com>
2026-06-29 16:12:53 +0900

    [프로그래머스] L0 - 배열 비교하기 (#143)

--- programmers/L0_기초입문/기초트레이닝/143_배열_비교하기.py
+++ programmers/L0_기초입문/기초트레이닝/143_배열_비교하기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181856
 # 알고리즘: 함수(메서드)
 # 작성자: 백하은
-# 작성일: 2026. 06. 29. 16:12:42
+# 작성일: 2026. 06. 29. 16:12:53
 
 def solution(arr1, arr2):
     if len(arr1) > len(arr2):
@@ -20,6 +20,7 @@ def solution(arr1, arr2):
             sum_of_arr1 += int(a)
         for b in arr2:
             sum_of_arr2 += int(b)
+            
         if sum_of_arr1 > sum_of_arr2:
             return 1
         elif sum_of_arr1 < sum_of_arr2:
