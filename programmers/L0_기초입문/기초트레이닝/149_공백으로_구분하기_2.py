e0f2902dad9ad3013e3512e2a70e8f22950b8fb9
hanalo26 <skyoung100@gmail.com>
2026-06-30 13:50:30 +0900

    [프로그래머스] L0 - 공백으로 구분하기 2 (#149)

---
+++ programmers/L0_기초입문/기초트레이닝/149_공백으로_구분하기_2.py
@@ -0,0 +1,10 @@
+# 공백으로 구분하기 2
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181868
+# 알고리즘: 문자열
+# 작성자: 백하은
+# 작성일: 2026. 06. 30. 13:50:30
+
+def solution(my_string):
+    answer = my_string.split()
+    return answer
