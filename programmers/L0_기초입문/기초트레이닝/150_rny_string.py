3ebc151cecac155bf66302bd7ec0177a6b6376fc
hanalo26 <skyoung100@gmail.com>
2026-06-30 13:57:43 +0900

    [프로그래머스] L0 - rny_string (#150)

---
+++ programmers/L0_기초입문/기초트레이닝/150_rny_string.py
@@ -0,0 +1,10 @@
+# rny_string
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181863
+# 알고리즘: 문자열
+# 작성자: 백하은
+# 작성일: 2026. 06. 30. 13:57:42
+
+def solution(rny_string):
+    answer = rny_string.replace('m','rn')
+    return answer
