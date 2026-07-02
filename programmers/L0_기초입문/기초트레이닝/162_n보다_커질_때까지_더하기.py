ba338fb37d6a4b34b9e2ea9c2f1b7a0334dcac39
hanalo26 <skyoung100@gmail.com>
2026-07-02 17:13:25 +0900

    [프로그래머스] L0 - n보다 커질 때까지 더하기 (#162)

---
+++ programmers/L0_기초입문/기초트레이닝/162_n보다_커질_때까지_더하기.py
@@ -0,0 +1,17 @@
+# n보다 커질 때까지 더하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181884
+# 알고리즘: 리스트(배열)
+# 작성자: 백하은
+# 작성일: 2026. 07. 02. 17:13:24
+
+def solution(numbers, n):
+    answer = 0
+    
+    for num in numbers:
+        answer += num
+        
+        if answer > n:
+            return answer
+    
+    return answer
