d3dc4ff66737c2173dec6eca1f2ff0da0cca68f8
hanalo26 <skyoung100@gmail.com>
2026-07-02 17:16:22 +0900

    [프로그래머스] L0 - 순서 바꾸기 (#163)

--- programmers/L0_기초입문/기초트레이닝/163_순서_바꾸기.py
+++ programmers/L0_기초입문/기초트레이닝/163_순서_바꾸기.py
@@ -3,11 +3,13 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181891
 # 알고리즘: 리스트(배열)
 # 작성자: 백하은
-# 작성일: 2026. 07. 02. 17:15:59
+# 작성일: 2026. 07. 02. 17:16:22
 
 def solution(num_list, n):
+    # n번째 원소 이후의 원소
     after_n = num_list[n:]
     
+    # n번째 원소 이전의 원소
     before_n = num_list[:n]
     
     return after_n + before_n
