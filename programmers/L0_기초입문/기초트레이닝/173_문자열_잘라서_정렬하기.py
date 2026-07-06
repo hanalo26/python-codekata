fa892337d82087da1666db9e4b5300e9b9e60ebb
hanalo26 <skyoung100@gmail.com>
2026-07-06 14:42:38 +0900

    [프로그래머스] L0 - 문자열 잘라서 정렬하기 (#173)

---
+++ programmers/L0_기초입문/기초트레이닝/173_문자열_잘라서_정렬하기.py
@@ -0,0 +1,16 @@
+# 문자열 잘라서 정렬하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181866
+# 알고리즘: 문자열
+# 작성자: 백하은
+# 작성일: 2026. 07. 06. 14:42:36
+
+def solution(myString):
+    
+    # "x"를 기준으로 문자열 자르기
+    split_myString = myString.split("x")
+    
+    # 빈 문자열이 아닌 요소만 골라서 정렬
+    answer = sorted([txt for txt in split_myString if txt != ""])
+    
+    return answer
