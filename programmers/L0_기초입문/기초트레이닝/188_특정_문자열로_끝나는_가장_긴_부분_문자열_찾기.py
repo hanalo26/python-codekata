41106b02a24f5069112dccb2fc7c77401e10089c
hanalo26 <skyoung100@gmail.com>
2026-07-08 14:12:18 +0900

    [프로그래머스] L0 - 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기 (#188)

---
+++ programmers/L0_기초입문/기초트레이닝/188_특정_문자열로_끝나는_가장_긴_부분_문자열_찾기.py
@@ -0,0 +1,12 @@
+# 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181872
+# 알고리즘: 문자열
+# 작성자: 백하은
+# 작성일: 2026. 07. 08. 14:12:17
+
+def solution(myString, pat):
+    # rfind(): 찾고자 하는 문자열이 마지막으로 등장하는 시작 인덱스를 반환
+    start_idx = myString.rfind(pat)
+    
+    return myString[:start_idx + len(pat)]
