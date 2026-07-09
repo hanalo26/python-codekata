6ff9ae79d0fa40d05b913cba4db7c7907c7a4d31
hanalo26 <skyoung100@gmail.com>
2026-07-09 15:59:11 +0900

    [프로그래머스] L0 - 간단한 논리 연산 (#195)

--- programmers/L0_기초입문/기초트레이닝/195_간단한_논리_연산.py
+++ programmers/L0_기초입문/기초트레이닝/195_간단한_논리_연산.py
@@ -3,10 +3,10 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181917
 # 알고리즘: 조건문
 # 작성자: 백하은
-# 작성일: 2026. 07. 09. 15:58:35
+# 작성일: 2026. 07. 09. 15:59:11
 
-# ∨ (OR 연산)
-# ∧ (AND 연산)
+# ∨ (파이썬에서의 OR 연산)
+# ∧ (파이썬에서의 AND 연산)
 
 def solution(x1, x2, x3, x4):
-    return (x1 or x2) and (x3 or x4)
+    return (x1 or x2) and (x3 or x4) 
