5be446d0751a3920318e93a97c38efe8ff755855
hanalo26 <skyoung100@gmail.com>
2026-07-09 16:01:12 +0900

    [프로그래머스] L0 - 문자열 반복해서 출력하기 (#196)

---
+++ programmers/L0_기초입문/기초트레이닝/196_문자열_반복해서_출력하기.py
@@ -0,0 +1,11 @@
+# 문자열 반복해서 출력하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181950
+# 알고리즘: 출력
+# 작성자: 백하은
+# 작성일: 2026. 07. 09. 16:01:11
+
+str, n = input().strip().split(' ')
+n = int(n)
+
+print(str*n)
