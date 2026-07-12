56b66482dc91d78c10460c9b217fc140dfe9961b
hanalo26 <skyoung100@gmail.com>
2026-07-12 16:41:14 +0900

    [프로그래머스] L0 - 무작위로 K개의 수 뽑기 (#211)

--- programmers/L0_기초입문/기초트레이닝/211_무작위로_K개의_수_뽑기.py
+++ programmers/L0_기초입문/기초트레이닝/211_무작위로_K개의_수_뽑기.py
@@ -3,21 +3,24 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181858
 # 알고리즘: 리스트(배열)
 # 작성자: 백하은
-# 작성일: 2026. 07. 12. 16:39:47
+# 작성일: 2026. 07. 12. 16:41:12
 
 def solution(arr, k):
     answer = []
     
     for num in arr:
+		    # answer에 포함된 값이면 건너뜀
         if num in answer:
             continue
         else:
             answer.append(num)
         
+        # answer의 길이가 k와 같아지면 즉시 stop
         if len(answer) == k:
             return answer
-        
+    
+    # k보다 짧다면 -1을 채워 길이가 k가 되도록 함    
     while len(answer) < k:
         answer.append(-1)
     
-    return answer
+    return answer 
