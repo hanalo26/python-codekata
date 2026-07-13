02ef47589519843768db91d16a1e726abff05c52
hanalo26 <skyoung100@gmail.com>
2026-07-13 11:53:43 +0900

    [프로그래머스] L0 - 정사각형으로 만들기 (#215)

---
+++ programmers/L0_기초입문/기초트레이닝/215_정사각형으로_만들기.py
@@ -0,0 +1,25 @@
+# 정사각형으로 만들기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181830
+# 알고리즘: 이차원 리스트(배열)
+# 작성자: 백하은
+# 작성일: 2026. 07. 13. 11:53:40
+
+def solution(arr):
+    rows = len(arr) # 행의 개수
+    cols = len(arr[0]) # 열의 개수
+    
+    # 행의 개수 > 열의 개수
+    if rows > cols:
+        diff = rows - cols
+        for r in arr:
+            r.extend([0]*diff)
+            
+    # 행의 개수 < 열의 개수
+    if rows < cols:
+        diff = cols - rows
+        for _ in range(diff):
+            arr.append([0]*cols)
+            
+    # 행의 개수 = 열의 개수
+    return arr
