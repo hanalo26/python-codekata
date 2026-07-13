c070931042d903b3d8b603112e924375090db548
hanalo26 <skyoung100@gmail.com>
2026-07-13 11:41:22 +0900

    [프로그래머스] L0 - a와 b 출력하기 (#213)

---
+++ programmers/L0_기초입문/기초트레이닝/213_a와_b_출력하기.py
@@ -0,0 +1,10 @@
+# a와 b 출력하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181951
+# 알고리즘: 출력
+# 작성자: 백하은
+# 작성일: 2026. 07. 13. 11:41:21
+
+a, b = map(int, input().strip().split(' '))
+print(f"a = {a}")
+print(f"b = {b}")
