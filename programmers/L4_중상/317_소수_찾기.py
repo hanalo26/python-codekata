7f1ac74e5b68fbd0cf22ea76c932b580b2e3d3ed
hanalo26 <skyoung100@gmail.com>
2026-07-22 23:57:30 +0900

    [프로그래머스] L4 - 소수 찾기 (#317)

--- programmers/L4_중상/317_소수_찾기.py
+++ programmers/L4_중상/317_소수_찾기.py
@@ -3,42 +3,46 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42839
 # 알고리즘: 완전탐색, 수학
 # 작성자: 백하은
-# 작성일: 2026. 07. 22. 21:56:38
+# 작성일: 2026. 07. 22. 23:57:30
 
-# 순열
 from itertools import permutations
 
-# 소수를 판별하는 함수
-# 1보다 큰 자연수 중에서 약수가 1과 자기 자신만 있는 수
-def is_prime(n):
-    if n < 2:
+# 어떤 숫자 x가 소수(prime number)인지 아닌지 판단하는 함수
+def is_prime(x):
+    # 조건
+    # (1) 2이상의 정수일 것
+    if x < 2:
         return False
-    
-    # 2부터 n의 제곱근까지의 정수 중에서 나누어 떨어지는 수가 있는지 확인
-    for i in range(2,int(n**0.5)+1):
-        if n % i == 0:
+    # (2) 약수가 자기자신과 1뿐일 것
+    for n in range(2,int(x**0.5)+1):
+        if x % n == 0:
             return False
     
     return True
 
-
+# numbers에서 소수의 개수를 반환하는 함수
 def solution(numbers):
-    # numbers에 있는 숫자카드 조합으로 만들 수 있는 모든 숫자(중복 제외)
+    nums = list(numbers)
+    
+    # numbers에 있는 요소로 만들 수 있는 모든 숫자(중복 제외)
     all_nums = set()
     
+    # 사용할 수 있는 요소의 개수
     n = len(numbers)
     
-    # 한 자릿수 ~ n 자릿수 숫자까지의 모든 순열 조합 제작
-    for i in range(1, n+1):
-        for m in permutations(numbers, i):
-            num = int("".join(m))
-            all_nums.add(num)
+    # 만드는 숫자의 자릿수
+    for i in range(1,n+1):
+        # 순열 조합 시작!
+        for m in permutations(nums,i):
+            # 튜플 형태로 반환되기 때문에 int로 변환
+            k = int("".join(m))
+            all_nums.add(k)
             
-    # all_nums 중에서 소수의 개수 찾기
+    # all_nums에서 소수의 개수 count
     answer = 0
     
-    for x in all_nums:
-        if is_prime(x):
+    for y in all_nums:
+        if is_prime(y):
             answer += 1
             
     # 최종 답
