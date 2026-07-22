b48ff0c2ecf9e2be1595372ef289d39c412bbf1a
hanalo26 <skyoung100@gmail.com>
2026-07-23 00:54:15 +0900

    [프로그래머스] L3 - 모음사전 (#310)

--- programmers/L3_중급/310_모음사전.py
+++ programmers/L3_중급/310_모음사전.py
@@ -3,37 +3,37 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/84512
 # 알고리즘: 완전탐색
 # 작성자: 백하은
-# 작성일: 2026. 07. 22. 23:13:19
+# 작성일: 2026. 07. 23. 00:54:14
 
 def solution(word):
-    # 각 단어를 구성하는 알파벳 모음
+    # 알파벳 모음
     base = ['A', 'E', 'I', 'O', 'U']
     
-    # 제작한 단어 저장
-    dictionary = []
+    # 단어 사전
+    words = []
     
-    # 단어를 제작하는 함수(dfs)
-    def dfs(curr_word):
-        # 1. 단어의 길이가 5를 초과하는가?
-        if len(curr_word) > 5:
-            # 다시 처음으로 돌아감
+    # 모음을 하나씩 이어붙이면서 단어 사전을 만드는 함수
+    def make_words(current_word):
+        # 입력된 단어의 길이가 5 미만인가?
+        # - 5보다 길면 처음으로 돌아가야 함
+        if len(current_word) > 5:
             return
         
-        # 2. 현재 만들어진 단어가 빈 문자열이 아닌가?
-        if curr_word != "":
-            # 2-1. dictionary에 단어 추가
-            dictionary.append(curr_word)
-            
-        # 3. A ~ UUUUU까지 뒤에 하나씩 붙여가면서 모든 단어 조합 생성
-        for v in base:
-            # curr_word에 v를 하나씩 붙인 단어를 dictionary에 추가
-            dfs(curr_word+v) # 단어장에서는 단어들이 (2)에 의해 계속 추가됨
-            
-    # 첫 시작은 빈 문자열부터
-    dfs("")
+        # 입력된 단어가 빈 문자열인가?
+        # - 빈 문자열이 아니라면 단어 사전에 추가함
+        if current_word != "":
+            words.append(current_word)
+        
+        # 모든 단어 조합 생성
+        for w in base:
+            # 두 조건문을 활용해 단어 사전에 계속 추가
+            make_words(current_word+w)
+        
+    # 내부 함수 사용, 시작은 빈 문자열
+    make_words("")
     
+    # 사전 내에서 몇 번째 단어인지 찾기
+    answer = words.index(word) + 1
     
-    # 주어진 단어가 사전에서 몇 번째에 있는가?
-    answer = dictionary.index(word) + 1
-    
+    # 답
     return answer
