e43fa86dbf325264a8bd6b87778307753ac1c96e
hanalo26 <skyoung100@gmail.com>
2026-07-22 18:40:49 +0900

    [프로그래머스] L4 - 네트워크 (#357)

--- programmers/L4_중상/357_네트워크.py
+++ programmers/L4_중상/357_네트워크.py
@@ -3,34 +3,36 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/43162
 # 알고리즘: DFS/BFS
 # 작성자: 백하은
-# 작성일: 2026. 07. 22. 16:43:45
+# 작성일: 2026. 07. 22. 18:40:48
 
 from collections import deque
 
 def solution(n, computers):
-    # 초기값
-    # 컴퓨터별 확인 현황
+    # 컴퓨터 확인 여부 (coms)
     coms = [False] * n
     
-    # 네트워크의 개수
+    # 연결된 네트워크 개수 (answer)
     answer = 0
     
     # 컴퓨터 번호별로 연결된 네트워크 확인
-    for idx in range(n):
-        # 아직 확인하지 않은 컴퓨터 발견!
-        if not coms[idx]:
+    for i in range(n):
+        # 동일한 컴퓨터끼리 확인하는 것도 네트워크 하나로 check
+        # 확인하지 않은 컴퓨터 발견!
+        if not coms[i]:
             answer += 1
-            que = deque([idx])
-            coms[idx] = True
+            # que에 컴퓨터 번호 저장
+            que = deque([i])
+            coms[i] = True # 확인한 컴퓨터라는 의미
         
-            # 이 컴퓨터와 연결된 모든 컴퓨터 확인
+            # i번째 컴퓨터와 다른 컴퓨터의 연결 확인
             while que:
                 com = que.popleft()
-
-                for next_com in range(n):
-                    # 연결된 컴퓨터이지만 아직 확인하지 않은 경우 
-                    if computers[com][next_com] == 1 and not coms[next_com]:
-                        coms[next_com] = True
-                        que.append(next_com)
-                    
+                
+                # 다른 컴퓨터와의 연결 확인
+                for other_com in range(n):
+                    # i번째 컴퓨터와 연결된 컴퓨터이지만, 아직 확인하지 않은 경우 (즉, i번째 컴퓨터가 아닌 경우)
+                    if computers[com][other_com] == 1 and (not coms[other_com]):
+                        coms[other_com] = True
+                        que.append(other_com)
     return answer
+    
