4bdb345abc22a9a05981c5af0002e5c849085f3f
hanalo26 <skyoung100@gmail.com>
2026-07-22 14:12:54 +0900

    [프로그래머스] L3 - 최소직사각형 (#257)

--- programmers/L3_중급/257_최소직사각형.py
+++ programmers/L3_중급/257_최소직사각형.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/86491
 # 알고리즘: 완전탐색, 그리디
 # 작성자: 백하은
-# 작성일: 2026. 07. 22. 14:12:25
+# 작성일: 2026. 07. 22. 14:12:53
 
 def solution(sizes):
     max_w = 0
@@ -17,5 +17,7 @@ def solution(sizes):
         # max_w, max_h와 비교해서 더 큰 값을 max_w, max_h로 저장
         max_w = max(max_w, w_side)
         max_h = max(max_h, h_side)
-        
-    return max_w * max_h
+    
+    answer = max_w * max_h
+    
+    return answer
