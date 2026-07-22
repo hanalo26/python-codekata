f55f83aefe6cd789c1270b09d77c7656f1c91c1b
hanalo26 <skyoung100@gmail.com>
2026-07-22 19:53:26 +0900

    [프로그래머스] L4 - 피로도 (#306)

--- programmers/L4_중상/306_피로도.py
+++ programmers/L4_중상/306_피로도.py
@@ -3,40 +3,47 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/87946
 # 알고리즘: 완전탐색, 백트래킹
 # 작성자: 백하은
-# 작성일: 2026. 07. 22. 17:43:24
+# 작성일: 2026. 07. 22. 19:53:25
 
 def solution(k, dungeons):
-    # 탐험할 수 있는 던전의 수 
-    # -1은 절대 나올 수 없으므로 디버깅을 위해 초기값으로 설정
-    answer = -1
+    # 초기값 설정
+    # 플레이어가 탐험할 수 있는 던전의 수
+    answer = -1 # 디버깅을 위해 절대 나올 수 없는 수로 정의
     
-    # 던전의 수
+    # 전체 던전의 수
     n = len(dungeons)
     
     # 던전 방문 여부
     visit = [False] * n
     
-    # current_k: 현재 피로도
-    # cnt: 탐험한 던전 수
-    def dfs(current_k, cnt):
-        max_dun = cnt
+    # 각 조합별로 몇 개의 던전 탐색이 가능한지 탐색
+    # 현재 피로도(current_k)
+    # 지금까지 탐험한 던전의 수(cnt) 
+    def dfs(corrent_k, cnt):
+        # 최대 탐험 가능한 던전의 수(max_cnt)
+        max_cnt = cnt
         
-        # 갈 수 있는 던전 탐색
+        # 각 던전별로 진입할 수 있는지 확인
+        # 1. 각 던전별로 진입 조건, 소모되는 k 확인
         for i in range(n):
-            min_k, use_k = dungeons[i]
+            min_k, used_k = dungeons[i]
             
-            # 방문하지 않았으며, 최소 피로도 조건을 채운 경우
-            if (not visit[i]) and (current_k >= min_k):
-                visit[i] = True # 방문함!
+            # 2. 이미 방문한 곳이 아니고, 진입 조건을 만족하는지 확인
+            if (not visit[i]) and (corrent_k >= min_k):
+                # 조건을 만족한다면 방문했다고 표시
+                visit[i] = True
                 
-                # 피로도 깎기, 도전한 던전은 +1
-                result = dfs(current_k-use_k, cnt+1)
-                max_dun = max(max_dun, result)
+                # 피로도는 깎이고, 탐험한 던전 수는 +1
+                result = dfs(corrent_k-used_k, cnt+1)
                 
-                # 도전한 던전이라는 표시 삭제
+                # 3. 탐험한 최대 던전 수 갱신
+                max_cnt = max(result, max_cnt)
+                
+                # 4. 던전 방문 여부 초기화
                 visit[i] = False
                 
-        # 찾아본 줄기에서 찾은 최대 던전 수
-        return max_dun
+        # 최대 던전 수 return
+        return max_cnt
     
-    return dfs(k,0)
+    # dfs() 함수를 활용한 값 return
+    return dfs(k,0)                
