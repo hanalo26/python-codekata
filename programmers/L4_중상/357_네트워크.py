06ea8ae6dd48cdbee93fa048a95e827f4001b9f3
hanalo26 <skyoung100@gmail.com>
2026-07-22 18:42:25 +0900

    [프로그래머스] L4 - 네트워크 (#357)

--- programmers/L4_중상/357_네트워크.py
+++ programmers/L4_중상/357_네트워크.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/43162
 # 알고리즘: DFS/BFS
 # 작성자: 백하은
-# 작성일: 2026. 07. 22. 18:40:48
+# 작성일: 2026. 07. 22. 18:42:24
 
 from collections import deque
 
@@ -32,7 +32,7 @@ def solution(n, computers):
                 for other_com in range(n):
                     # i번째 컴퓨터와 연결된 컴퓨터이지만, 아직 확인하지 않은 경우 (즉, i번째 컴퓨터가 아닌 경우)
                     if computers[com][other_com] == 1 and (not coms[other_com]):
-                        coms[other_com] = True
-                        que.append(other_com)
+                        coms[other_com] = True # 같은 네트워크로 연결되었음을 확인
+                        que.append(other_com) # 그 컴퓨터와 연결된 또 다른 컴퓨터를 찾기 위해 추가
     return answer
     
