5a5fded0a324b6201e6a8d319347dd2e76f1d4d2
hanalo26 <skyoung100@gmail.com>
2026-07-22 16:43:04 +0900

    [프로그래머스] L4 - 네트워크 (#357)

--- programmers/L4_중상/357_네트워크.py
+++ programmers/L4_중상/357_네트워크.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/43162
 # 알고리즘: DFS/BFS
 # 작성자: 백하은
-# 작성일: 2026. 07. 22. 16:42:27
+# 작성일: 2026. 07. 22. 16:43:03
 
 from collections import deque
 
@@ -17,7 +17,7 @@ def solution(n, computers):
     
     # 컴퓨터 번호별로 연결된 네트워크 확인
     for idx in range(n):
-        # 아직 연결하지 않은 컴퓨터 발견!
+        # 아직 확인하지 않은 컴퓨터 발견!
         if not coms[idx]:
             answer += 1
             que = deque([idx])
