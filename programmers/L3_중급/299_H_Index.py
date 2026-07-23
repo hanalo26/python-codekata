c9b2a219ae19799a6130da15fc0048d52ee04a47
hanalo26 <skyoung100@gmail.com>
2026-07-24 00:47:53 +0900

    [프로그래머스] L3 - H-Index (#299)

--- programmers/L3_중급/299_H_Index.py
+++ programmers/L3_중급/299_H_Index.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42747
 # 알고리즘: 정렬
 # 작성자: 백하은
-# 작성일: 2026. 07. 24. 00:47:20
+# 작성일: 2026. 07. 24. 00:47:53
 
 # h: 어느 과학자의 H-Index를 나타내는 값
 # h번 이상 인용된 논문이 h편 이상 있고, 그렇지 않은 것도 h편 이하로 있다 -> 이떄의 h가 H-Index가 됨
@@ -19,7 +19,7 @@ def solution(citations):
         h = idx + 1
         
         if citations[idx] < h:
-            return h - 1
+            return h-1
             
     # 반복문이 종료되었음에도 불구하고 조건이 깨지지 않는다면?
     return len(citations)
