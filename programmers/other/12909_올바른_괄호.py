faabd4bedaf861a98db24701475635e37554fc38
hanalo26 <skyoung100@gmail.com>
2026-07-23 22:17:01 +0900

    [프로그래머스] 올바른 괄호 풀이 제출
    - 문제 ID: 12909
    - 난이도: unknown
    - 제출일: 2026. 07. 23. 22:17:00
    - 작성자: 백하은

--- programmers/other/12909_올바른_괄호.py
+++ programmers/other/12909_올바른_괄호.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12909
 # 작성자: 백하은
-# 작성일: 2026. 07. 23. 22:16:41
+# 작성일: 2026. 07. 23. 22:17:00
 
 # 올바른 괄호가 되기 위해서는 ( 등장 -> ) 등장
 # 닫는 괄호가 나오기 전에 여는 괄호가 쌓여있어야 함 -> 만약에 없다면 False
@@ -10,12 +10,10 @@
 
 def solution(s):
     opened = []
-    n = 0
     
     for i in s:
         if i == "(":
             opened.append(i)
-        
         elif opened:
                 opened.pop() # 맨 뒤에 있는 요소 제거
         else:
