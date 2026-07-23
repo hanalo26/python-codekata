7bce49b5d81179d518ae247d48aa299fbb8d309d
hanalo26 <skyoung100@gmail.com>
2026-07-23 17:26:22 +0900

    [프로그래머스] 구명보트 풀이 제출
    - 문제 ID: 42885
    - 난이도: unknown
    - 제출일: 2026. 07. 23. 17:26:21
    - 작성자: 백하은

--- programmers/other/42885_구명보트.py
+++ programmers/other/42885_구명보트.py
@@ -2,35 +2,30 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42885
 # 작성자: 백하은
-# 작성일: 2026. 07. 23. 15:17:11
-
-# 구해야 하는 것: 모든 사람을 구출하기 위해 필요한 최소한의 구명보트 수
-
-# 가장 무거운 사람은 가장 가벼운 사람과 함께 탈 수 없더라도 무조건 해당 순서에 혼자서라도 떠난다!
+# 작성일: 2026. 07. 23. 17:26:21
 
 def solution(people, limit):
+    # 1. 인원 수(n)
+    n = len(people)
     
-    # 1. 몸무게가 가벼운 순서대로 배치
+    # 2. 몸무게 기준으로 정렬
     people.sort()
     
-    # 2. 남은 사람 중에 가장 가벼운 사람의 번호(left)와 가장 무거운 사람의 번호(right)라 한다.
-    left = 0
-    right = len(people) - 1
+    # 3. 가장 무거운 사람과 가장 가벼운 사람의 번호
+    light = 0
+    heavy = n-1
     
-    # 3. 사용한 구명보트의 개수
-    cnt = 0
+    # 4. 구명보트 개수
+    boat = 0
     
-    # 4. 가장 가벼운 사람 = 가장 무거운 사람, 즉 혼자 남을 때까지 반복
-    while left <= right:
-    
-        # 5. 가장 가벼운 사람과 가장 무거운 사람이 함께 떠날 수 있는지 확인
-        if people[left] + people[right] <= limit:
-            
-            left += 1
+    # 5. 탐색
+    while light <= heavy:
+        if people[light] + people[heavy] <= limit:
+            light += 1
         
-        # 6. 가장 무거운 사람 혼자 떠나야 하는 경우(같이 타더라도 보트 수는 무조건 +1)
-        right = right - 1
-        cnt += 1
-        
-    # 7. 최종 답
-    return cnt
+        # 무거운 사람은 무조건 탈출함
+        heavy = heavy - 1
+        boat += 1
+    
+    # 6. 답
+    return boat
