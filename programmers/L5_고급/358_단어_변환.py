bca972ab83775dbdffb585b34255c785570de1de
hanalo26 <skyoung100@gmail.com>
2026-07-24 15:27:57 +0900

    [프로그래머스] L5 - 단어 변환 (#358)

--- programmers/L5_고급/358_단어_변환.py
+++ programmers/L5_고급/358_단어_변환.py
@@ -3,47 +3,41 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/43163
 # 알고리즘: DFS/BFS
 # 작성자: 백하은
-# 작성일: 2026. 07. 22. 20:11:54
+# 작성일: 2026. 07. 24. 15:27:57
 
 from collections import deque
 
 def solution(begin, target, words):
-    # 1. target 단어가 words에 포함되어 있는지 확인
+    # 1. target not in words -> return 0
     if target not in words:
         return 0
     
-    # 2. words에 있는 각 단어를 이미 사용했는지 확인하는 리스트 정의
+    # 2. 이미 사용한 단어는 사용 불가 -> 사용하면 False를 True로 변경
     n = len(words)
-    visited = [False] * n
+    visit = [False] * n
     
-    # 3. (현재 단어, 변환 횟수) 조합으로 초기 que 정의
-    que = deque([(begin, 0)])
+    # 3. (현재 단어, 변환 횟수)를 담은 큐 생성
+    q = deque([(begin,0)])
     
-    # 4. 탐색 시작
-    while que:
-        current_word, cnt = que.popleft()
-    
-        # 5. 현재 단어 == target 단어인 경우
+    # 4. 탐색
+    # 현재단어와 사용하지 않은 word 내 단어의 알파벳을 비교해서 diff가 1인 단어로 변경하고 다시 큐에 저장
+    while q:
+        current_word, cnt = q.popleft()
+        
         if current_word == target:
             return cnt
         
-        # 6. 현재 단어 != target 단어인 경우
-        # 7. words에 남은 단어 탐색
-        for i in range(n):
-            next_word = words[i]
-            # 8. 현재 단어와 새로운 단어간의 다른 알파벳 개수 카운트
-            # (단, 앞서 사용한 단어가 아니어야 함)
-            if not visited[i]:
-                # 8-1. 두 단어 간의 서로 다른 알파벳 개수(diff)
+        else:
+            for i in range(n):
+                next_word = words[i]
+                
+                if not visit[i]:
                     diff = 0
                     for a1, a2 in zip(current_word,next_word):
                         if a1 != a2:
                             diff += 1
-                    # 8-2. diff == 1인 경우엔 그 단어로 이동 
                     if diff == 1:
-                        visited[i] = True
-                        que.append((next_word, cnt+1))
+                        visit[i] = True
+                        q.append((next_word,cnt+1))
                         
-                    
-    # 9. 모든 단어를 훑었음에도 실패한 경우
     return 0
