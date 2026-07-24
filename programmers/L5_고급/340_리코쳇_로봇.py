7cd875bb7998334254ea429af2457176be010883
hanalo26 <skyoung100@gmail.com>
2026-07-24 21:14:10 +0900

    [프로그래머스] L5 - 리코쳇 로봇 (#340)

--- programmers/L5_고급/340_리코쳇_로봇.py
+++ programmers/L5_고급/340_리코쳇_로봇.py
@@ -3,25 +3,28 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/169199
 # 알고리즘: BFS
 # 작성자: 백하은
-# 작성일: 2026. 07. 23. 16:58:55
+# 작성일: 2026. 07. 24. 21:14:09
 
+# 로봇은 상하좌우로 움직임
+# 로봇은 벽이나 장애물에 부딪히기 전까지 한 방향으로 이동함
+# 구해야 하는 것: 목표지점에 도착하기 위해 움직여야 하는 횟수
+## 단, 도착할 수 없다면 -1 반환
 from collections import deque
 
 def solution(board):
-    # 1. 격자의 크기는?
-    n = len(board) # 행
-    m = len(board[0]) # 열
+    # 격자의 크기
+    n = len(board)
+    m = len(board[0])
     
-    # 2. 시작지점과 목표지점은?
+    # 출발지점과 목표지점 위치 정의
     for i in range(n):
         for j in range(m):
             if board[i][j] == "R":
                 start = (i,j)
-            if board[i][j] == "G":
+            elif board[i][j] == "G":
                 goal = (i,j)
     
-    # 3. 미끄러져서 로봇이 서있는 지점 기록, 상하좌우로 움직인 횟수 등을 기록하기 위한 변수 정의 + 이동을 위한 방향키 설정
-    # 방향키(상하좌우 순서대로)
+    # 상하좌우 이동을 위한 좌표 []
     directions = [
         (-1,0),
         (1,0),
@@ -29,35 +32,37 @@ def solution(board):
         (0,1)
     ]
     
-    # 방문여부 기록용
-    visited = {start}
+    # 방문한 점을 기록하는 {}
+    visit = {start}
     
-    # 좌표 및 이동횟수 기록용
-    q = deque([(start,0)])
+    # 현재 위치, 이동 횟수를 기록하는 que
+    que = deque([(start,0)])
     
-    # 4. 탐색 (탐색 중에 목표지점에 도달하면 종료)
-    while q:
-        (r,c),cnt = q.popleft()
+    # 반복문
+    while que:
+        # 현재 위치와 이동횟수를 꺼낼 것
+        (r, c), cnt = que.popleft()
         
-        # (r,c)가 목표 지점일 때
+        # que에서 꺼낸 좌표가 목표지점인지 확인
         if (r,c) == goal:
             return cnt
         
-        # (r,c)가 목표 지점이 아닐 떄
+        # 목표지점이 아니라면?
         for dr, dc in directions:
-            # 한 칸 이동하자마자 벽이나 장애물에 부딪혔을 떄
+            # 한 칸만 이동하고 벽이나 장애물에 막힐 수 있음
             nr = r
             nc = c
             
-            # 한 칸 이동했을 때 벽이나 장애물에 부딪히지 않았을 때 -> 부딪힐 때까지 이동
-            while 0 <= nr+dr < n and 0 <= nc+dc < m and board[nr+dr][nc+dc] != "D":
-                nr = nr+dr
-                nc = nc+dc
-                
-            # 방문했다고 기록되지 않은 칸이라면?
-            if (nr,nc) not in visited:
-                visited.add((nr,nc))
-                q.append(((nr,nc),cnt+1))
+            # 한 칸 더 이동해도 되는지 확인
+            while 0 <= nr + dr < n and 0 <= nc + dc < m and board[nr + dr][nc + dc] != "D":
+                nr = nr + dr
+                nc = nc + dc
+            
+            # 움직인 점의 좌표를 {}에 기록하고, que에 담음(단, 방문 이력이 없는 칸이어야 함)
+            if (nr,nc) not in visit:
+                visit.add((nr,nc))
+                que.append(((nr,nc),cnt+1))
+
     
-    # 5. 만약에 끝까지 목표지점을 만나지 못한다면?
+    # 끝까지 목표지점을 만나지 못한 경우
     return -1
