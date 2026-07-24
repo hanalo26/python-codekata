4da77c7b12029316f155c8d616494ebfce5f9b57
hanalo26 <skyoung100@gmail.com>
2026-07-24 18:06:51 +0900

    [프로그래머스] L3 - 카펫 (#292)

--- programmers/L3_중급/292_카펫.py
+++ programmers/L3_중급/292_카펫.py
@@ -3,21 +3,22 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42842
 # 알고리즘: 완전탐색, 수학
 # 작성자: 백하은
-# 작성일: 2026. 07. 23. 00:03:58
-
-# 중앙은 노란색, 테두리는 갈색인 카펫이 만들어짐
+# 작성일: 2026. 07. 24. 18:06:51
 
 def solution(brown, yellow):
-    # 전체 칸의 개수(total)
+    # 갈색이 테두리, 노란색이 중앙
+    
+    # 1. 카펫의 크기
     total = brown + yellow
     
-    # total = w*h인 정수 w와 h 찾기
-    # 조건 1. 가로 >= 세로
-    # 조건 2. 노란색이 한 줄짜리일때, 이를 감싸기 위해서는 최소 높이 3이 필요함
-    for h in range(3,total+1):
+    # 2. 높이에 따른 가로 계산
+    # - 높이는 중앙에 노란색이 한 줄만 있을 때, 최소 3개가 필요함
+    for h in range(3, total+1):
         if total % h == 0:
             w = total // h
             
-            # 가운데에 있는 노란 블록은 최소 테두리가 하나씩 양쪽에 존재하므로 yellow = (w-2)*(h-2)가 되어야 함
+            # - 가로는 항상 세로보다 길거나 같음
             if w >= h and (w-2)*(h-2) == yellow:
                 return [w,h]
+            
+    return -1
