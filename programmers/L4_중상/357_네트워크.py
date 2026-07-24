95e087e8f54d8b51355473cad832fb203002b585
hanalo26 <skyoung100@gmail.com>
2026-07-24 14:56:23 +0900

    [프로그래머스] L4 - 네트워크 (#357)

--- programmers/L4_중상/357_네트워크.py
+++ programmers/L4_중상/357_네트워크.py
@@ -3,36 +3,32 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/43162
 # 알고리즘: DFS/BFS
 # 작성자: 백하은
-# 작성일: 2026. 07. 22. 18:42:24
+# 작성일: 2026. 07. 24. 14:56:22
 
 from collections import deque
 
 def solution(n, computers):
-    # 컴퓨터 확인 여부 (coms)
-    coms = [False] * n
-    
-    # 연결된 네트워크 개수 (answer)
+    # 네트워크 개수 (함수를 통해 구해야 하는 값!)
     answer = 0
     
-    # 컴퓨터 번호별로 연결된 네트워크 확인
-    for i in range(n):
-        # 동일한 컴퓨터끼리 확인하는 것도 네트워크 하나로 check
-        # 확인하지 않은 컴퓨터 발견!
-        if not coms[i]:
-            answer += 1
-            # que에 컴퓨터 번호 저장
-            que = deque([i])
-            coms[i] = True # 확인한 컴퓨터라는 의미
-        
-            # i번째 컴퓨터와 다른 컴퓨터의 연결 확인
-            while que:
-                com = que.popleft()
-                
-                # 다른 컴퓨터와의 연결 확인
-                for other_com in range(n):
-                    # i번째 컴퓨터와 연결된 컴퓨터이지만, 아직 확인하지 않은 경우 (즉, i번째 컴퓨터가 아닌 경우)
-                    if computers[com][other_com] == 1 and (not coms[other_com]):
-                        coms[other_com] = True # 같은 네트워크로 연결되었음을 확인
-                        que.append(other_com) # 그 컴퓨터와 연결된 또 다른 컴퓨터를 찾기 위해 추가
-    return answer
+    #0번부터 n-1번 컴퓨터까지 중 확인한 컴퓨터 목록(같은 번호의 컴퓨터를 2번 확인하지 않기 위해서 체크)
+    visited = [False] * n
     
+    # 컴퓨터 번호별 연결된 컴퓨터 확인
+    for num in range(n):
+        # 동일한 번호의 컴퓨터는 같은 네트워크에 연결된 것
+        if not visited[num]:
+            answer += 1
+            visited[num] = True
+            
+            q = deque([num])
+            
+            # num번째 컴퓨터와 다른 컴퓨터와의 연결 확인
+            while q:
+                com = q.popleft()
+                
+                for other in range(n):
+                    if computers[com][other] == 1 and not visited[other]:
+                        visited[other] = True
+                        q.append(other)
+    return answer
