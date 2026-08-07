a41b79f138b50e5beb2c1f6cdf88f686f3c054e7
hanalo26 <skyoung100@gmail.com>
2026-08-07 18:27:28 +0900

    [프로그래머스] L3 - n^2 배열 자르기 (#300)

---
+++ programmers/L3_중급/300_n_2_배열_자르기.py
@@ -0,0 +1,24 @@
+# n^2 배열 자르기
+# 프로그래머스 L3 (중급)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/87390
+# 알고리즘: 수학, 시뮬레이션
+# 작성자: 백하은
+# 작성일: 2026. 08. 07. 18:27:27
+
+# i는 1차원 배열의 인덱스
+# 행(Row) 번호: i // n
+# 열(Column) 번호: i % n
+
+# 2차원 좌표에서 각 위치에 대입하는 값
+# (r,c) -> max(r,c)+1
+
+def solution(n, left, right):
+    answer = []
+    
+    for i in range(left, right+1):
+        r = i // n
+        c = i % n
+        value = max(r,c) + 1
+        answer.append(value)
+    
+    return answer
