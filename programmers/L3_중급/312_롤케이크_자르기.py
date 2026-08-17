3cdb5a6fe2e3ec1a555ab7d1291a7bfc6c93f8f9
hanalo26 <skyoung100@gmail.com>
2026-08-17 16:45:48 +0900

    [프로그래머스] L3 - 롤케이크 자르기 (#312)

--- programmers/L3_중급/312_롤케이크_자르기.py
+++ programmers/L3_중급/312_롤케이크_자르기.py
@@ -3,31 +3,29 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/132265
 # 알고리즘: 해시
 # 작성자: 백하은
-# 작성일: 2026. 07. 25. 19:36:19
+# 작성일: 2026. 08. 17. 16:45:48
 
-# 케이크의 크기 -> 즉, 슬라이싱된 리스트의 길이는 상관 없음
-# 슬라이싱된 리스트에 동일한 가짓수의 토핑이 올라가기만 하면 됨 -> 구체적인 종류까지 같을 필요는 없음
-# 공평하게 나눌 수 없다면 0을 반환
 from collections import Counter
 
 def solution(topping):
     answer = 0
     
-    # 철수
-    Chul = set()
+    # 철수가 먹는 롤케이크에 올라가는 토핑 가짓수
+    Chu = set()
     
-    # 동생
+    # 동생이 먹는 롤케이크에 올라가는 토핑 가짓수
+    ## {토핑 종류:개수}의 형식으로 저장
     Bro = Counter(topping)
     
-    # 1. 칼로 자르는 위치를 옮겨가면서 확인
+    # 칼로 자르는 위치(=인덱스)를 바꿔가면서 테스트
     for i in topping:
-        Chul.add(i)
-        Bro[i] = Bro[i] - 1
+        Chu.add(i) # 토핑이 하나 생김
+        Bro[i] = Bro[i] - 1 # 토핑이 하나 사라짐
         
-        if Bro[i] == 0:
+        if Bro[i] == 0: # 모든 토핑 분배 완료
             del Bro[i]
-        
-        if len(Chul) == len(Bro):
+            
+        if len(Chu) == len(Bro):
             answer += 1
-
+    
     return answer
