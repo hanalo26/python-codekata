fb0c07e4d1fe5012421a92ce29141b856c05dc40
hanalo26 <skyoung100@gmail.com>
2026-08-20 14:09:49 +0900

    [프로그래머스] L4 - 피로도 (#306)

--- programmers/L4_중상/306_피로도.py
+++ programmers/L4_중상/306_피로도.py
@@ -3,40 +3,39 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/87946
 # 알고리즘: 완전탐색, 백트래킹
 # 작성자: 백하은
-# 작성일: 2026. 07. 24. 19:21:43
+# 작성일: 2026. 08. 20. 14:09:48
 
 def solution(k, dungeons):
-    
     # 도전할 수 있는 던전의 수
-    n = len(dungeons) 
+    n = len(dungeons)
     
     # 특정 던전 방문 여부
     visit = [False] * n
     
-    # 각 던전별로 클리어 가능 여부 탐색
-    # current_k : 특정 던전에 진입하기 전, 피로도
-    # cnt : 특정 던전에 진입하기 전까지 클리어한 던전의 수
-    def dfs(current_k, cnt):
+    # 각 던전별 클리어 가능 여부 탐색
+    ## cur_k : 던전에 진입하기 전 보유한 피로도
+    ## cnt : 직전까지 클리언한 던전의 수
+    def dfs(cur_k, cnt):
         
-        # 탐험 가능한 최대 던전 수
         max_cnt = cnt
         
-        # 각 던전 별로 확인
+        # 던전별로 클리어 가능 여부 확인
         for i in range(n):
-            min_k, used_k = dungeons[i]
+            min_k, used_k =  dungeons[i]
             
-            # 이미 방문한 던전이 아니고, 진입 조건을 만족하는지 검사
-            if (not visit[i]) and (current_k >= min_k):
+            # 진입 조건 만족 여부 검사
+            if (not visit[i]) and (cur_k >= min_k):
                 visit[i] = True
                 
-                result = dfs(current_k-used_k, cnt+1)
+                # 던전 클리어 후
+                result = dfs(cur_k-used_k, cnt+1)
                 
-                # 최대 던전 수 갱신 
+                # 던전 수 갱신
                 max_cnt = max(max_cnt, result)
                 
                 # 던전 방문 여부 초기화
                 visit[i] = False
-    
+                
         return max_cnt
     
     return dfs(k,0)
